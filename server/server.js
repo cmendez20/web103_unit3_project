ae444c0f0a30af5265a432a86f51895162e58916
chris mendez mba <mendecj812@gmail.com>
2025-10-02 16:17:32 -0500

    refactor: routes with api prefix

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import path from 'path';
-import favicon from 'serve-favicon';
-import dotenv from 'dotenv';
-import eventsRouter from './routes/eventsRouter.js';
-import locationsRouter from './routes/locationsRouter.js';
+import express from "express";
+import path from "path";
+import favicon from "serve-favicon";
+import dotenv from "dotenv";
+import eventsRouter from "./routes/eventsRouter.js";
+import locationsRouter from "./routes/locationsRouter.js";
 
 // import the router from your routes file
 
@@ -14,20 +14,20 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
-app.use('/events', eventsRouter);
-app.use('/locations', locationsRouter);
+app.use("/api/events", eventsRouter);
+app.use("/api/locations", locationsRouter);
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(favicon(path.resolve('../', 'client', 'public', 'party.png')));
-} else if (process.env.NODE_ENV === 'production') {
-  app.use(favicon(path.resolve('public', 'party.png')));
-  app.use(express.static('public'));
+if (process.env.NODE_ENV === "development") {
+  app.use(favicon(path.resolve("../", "client", "public", "party.png")));
+} else if (process.env.NODE_ENV === "production") {
+  app.use(favicon(path.resolve("public", "party.png")));
+  app.use(express.static("public"));
 }
 
 // specify the api path for the server to use
 
-if (process.env.NODE_ENV === 'production') {
-  app.get('/*', (_, res) => res.sendFile(path.resolve('public', 'index.html')));
+if (process.env.NODE_ENV === "production") {
+  app.get("/*", (_, res) => res.sendFile(path.resolve("public", "index.html")));
 }
 
 app.listen(PORT, () => {
